3e5870f5f1187370d9d74db3923171efa0c371cd
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-16 23:48:22 +0530

    play the game with 2 player.Player gets the ladder then play again

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,40 +5,77 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	    int pos = 0;
+	    int pos1 = 0;
+	    int pos2 = 0;
 	    int dice ;
 	    int temp;
-	    int countDice = 0;
-	    int countPos = 0;
+	    int countDice1 = 0;
+	    int countPos1 = 0;
+        int countDice2 = 0;
+        int countPos2 = 0;
         System.out.println("Welcome to Snake And Ladder program");
-        System.out.println("Single player at start position "+ pos);
-        while (pos <= 100){
-            dice = rollDice();
-            countDice++;
-            int t = pos;
-            pos = mapTo(dice, pos);
-            if(pos > t)
-                countPos++;
+        System.out.println("Player1 player at start position "+ pos1);
+        System.out.println("Player2 player at start position "+ pos2);
+        while (pos1 <= 100 || pos2 <= 100) {
+            Random random = new Random();
+            int play = random.nextInt(2);
+            play = play + 1;
+            if (play == 1) {
+                dice = rollDice();
+                countDice1++;
+                int t = pos1;
+                pos1 = mapTo(dice, pos1);
+                if (pos1 > t)
+                    countPos1++;
+                if (pos1 >= 100)
+                    break;
+            } else {
+                dice = rollDice();
+                countDice2++;
+                int t = pos2;
+                pos2 = mapTo(dice, pos2);
+                if (pos2 > t)
+                    countPos2++;
+                if (pos2 >= 100)
+                    break;
+            }
         }
-        if(pos > 100){
-            temp = pos - 100;
-            pos = pos - temp;
-            System.out.println("Win the game pos is : "+ pos);
-            System.out.println("No of Count Dice : "+ countDice);
-            System.out.println("No of Count Pos : "+ countPos);
 
-        }
-        else{
-            System.out.println("Win the game pos is : "+ pos);
-            System.out.println("No of Count Dice : "+ countDice);
-            System.out.println("No of Count Pos : "+ countPos);
+        if(pos1 > 100 || pos2 > 100){
+            int temp1 = pos1 - 100;
+            pos1 = pos1 - temp1;
+            int temp2 = pos2 - 100;
+            pos2 = pos2 - temp2;
+
+            if(pos1 == 100){
+                System.out.println("Win the game Player1 pos is : "+ pos1);
+                System.out.println("No of Count Player1 Dice : "+ countDice1);
+                System.out.println("No of Count Player1 Pos : "+ countPos1);
+
+            }else if(pos2 == 100){
+                System.out.println("Win the game Player2 pos is : "+ pos2);
+                System.out.println("No of Count Player2 Dice : "+ countDice2);
+                System.out.println("No of Count Player2 Pos : "+ countPos2);
+            }
+        }else {
+            if (pos1 == 100) {
+                System.out.println("Win the game Player1 pos is : " + pos1);
+                System.out.println("No of Count Player1 Dice : " + countDice1);
+                System.out.println("No of Count Player1 Pos : " + countPos1);
+            } else {
+                System.out.println("Win the game Player2 pos is : " + pos2);
+                System.out.println("No of Count Player2 Dice : " + countDice2);
+                System.out.println("No of Count Player2 Pos : " + countPos2);
+
+            }
         }
     }
+
+
     public static int rollDice(){
         Random random = new Random();
         int dice = random.nextInt(6);
         dice = dice+1;
-        //System.out.println("Dice Num : "+ dice);
         return dice;
     }
     public static int mapTo(int dice, int pos){
